6d59329c55514b58e0b1f49e4e33ed6354ee9e31
Kevin Lehmann <83122333+darksonic2112@users.noreply.github.com>
2024-02-29 17:01:23 -0900

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int points = 3;
+    public delegate void EnemyDied(int pointsWorth);
+    public static event EnemyDied OnEnemyDied;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+
+        OnEnemyDied.Invoke(points);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+  public Transform shottingOffset;
+
+    private void Start()
+    {
+        Enemy.OnEnemyDied += EnemyOnOnEnemyDied;
+    }
+
+    private void OnDestroy()
+    {
+        Enemy.OnEnemyDied -= EnemyOnOnEnemyDied;
+    }
+    void EnemyOnOnEnemyDied(int pointsWorth)
+    {
+        Debug.Log($"Player received 'EnemyDied' worth {pointsWorth}");
+    }
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        Destroy(shot, 3f);
+
+      }
+    }
+}
