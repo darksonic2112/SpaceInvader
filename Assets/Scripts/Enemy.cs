e2f918e353121e685c5f3cd92af78d79c17eff18
DARKSONIC <kevin-lehmann99@web.de>
2024-03-05 16:01:56 -0800

    Enemys move towards player, go fixed distance to the sides and speed up now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,29 +11,90 @@ public class Enemy : MonoBehaviour
     public delegate void EnemyDied(int pointsWorth);
     public static event EnemyDied OnEnemyDied;
     
+    private List<GameObject> enemies = new List<GameObject>();
     public float interval = 2f;
     private float timer;
     private float bulletSpeed = 10f;
     RaycastHit hit;
+    private float timeCounter;
+    private static float enemySpeedUp = 1f;
+    private float enemyMoveSpeed = 100f;
+    private float totalDistance;
+    private bool travelLeft;
+
+    private void Start()
+    {
+        enemySpeedUp = 1f;
+    }
 
     void Update()
     {
         timer += Time.deltaTime;
+        timeCounter += Time.deltaTime;
         
         if (timer >= interval)
         {
             timer = 0f;
             Fire();
         }
+        
+        if (travelLeft)
+        {
+            if (timeCounter > enemySpeedUp)
+            {
+                if ((int)totalDistance < -1200)
+                {
+                    travelLeft = false;
+                    Vector3 enemyDownStep = new Vector3(0, -enemyMoveSpeed * Time.deltaTime, 0);
+                    gameObject.transform.Translate(enemyDownStep);
+                }
+                else
+                {
+                    Vector3 enemyMoveStep = new Vector3(-enemyMoveSpeed * Time.deltaTime, 0, 0);
+
+                    gameObject.transform.Translate(enemyMoveStep);
+                    totalDistance -= enemyMoveSpeed;
+                }
+
+                timeCounter = 0.0f;
+            }
+        }
+        else
+        {
+            if (timeCounter > enemySpeedUp)
+            {
+                if ((int)totalDistance > 1500)
+                {
+                    travelLeft = true;
+                    Vector3 enemyDownStep = new Vector3(0, -enemyMoveSpeed * Time.deltaTime, 0);
+                    gameObject.transform.Translate(enemyDownStep);
+                }
+                else
+                {
+                    Vector3 enemyMoveStep = new Vector3(enemyMoveSpeed * Time.deltaTime, 0, 0);
+
+                    gameObject.transform.Translate(enemyMoveStep);
+                    totalDistance += enemyMoveSpeed;
+                }
+
+                timeCounter = 0.0f;
+            }
+        }
     }
     
     void OnCollisionEnter(Collision other)
     {
+        Debug.Log("Collision detected");
         if (other.gameObject.CompareTag("Bullet"))
         {
+            Debug.Log("speed before: " + enemySpeedUp);
+            enemySpeedUp -= 0.02f;
+            Debug.Log("speed after: " + enemySpeedUp);
             OnEnemyDied.Invoke(points);
+            
             Destroy(gameObject);
             Destroy(other.gameObject);
+            
         }
     }
     void Fire()
--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -15,12 +15,14 @@ public class EnemyBehaviour : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI highScoreText;
 
+    private List<GameObject> enemies = new List<GameObject>();
     private Enemy enemy;
     private int enemyRows = 6;
     private int enemyColumns = 3;
     private int score;
     private int highScore;
     private int maxBaricades = 10;
+    private int enemyAmount;
 
     private string highScoreFilePath = "highscore.txt";
 
@@ -36,17 +38,32 @@ public class EnemyBehaviour : MonoBehaviour
             {
                 Vector3 position = new Vector3(-4 + enemyAmountRow, 3 - enemyAmountColumn, -0.5f);
                 if (enemyAmountColumn == 0)
-                    Instantiate(enemyPrefab, position, Quaternion.identity);
+                {
+                    GameObject enemyInstance = Instantiate(enemyPrefab, position, Quaternion.identity);
+                    enemies.Add(enemyInstance);
+                }
+
                 if (enemyAmountColumn == 1)
-                    Instantiate(enemyPrefab1, position, Quaternion.identity);
+                {
+                    GameObject enemyInstance = Instantiate(enemyPrefab1, position, Quaternion.identity);
+                    enemies.Add(enemyInstance);
+                }
+
                 if (enemyAmountColumn == 2)
-                    Instantiate(enemyPrefab2, position, Quaternion.identity);
+                {
+                    GameObject enemyInstance = Instantiate(enemyPrefab2, position, Quaternion.identity);
+                    enemies.Add(enemyInstance);
+                }
+
                 if (enemyAmountColumn == 3)
-                    Instantiate(enemyPrefab3, position, Quaternion.identity);
+                {
+                    GameObject enemyInstance = Instantiate(enemyPrefab3, position, Quaternion.identity);
+                    enemies.Add(enemyInstance);
+                }
             }
         }
 
-        for (int baricadeAmount = 0; baricadeAmount <= maxBaricades; baricadeAmount+=2)
+        for (int baricadeAmount = 0; baricadeAmount <= maxBaricades; baricadeAmount += 2)
         {
             Vector3 baricadePosition1 = new Vector3(-5 + baricadeAmount, -3.25f, -0.5f);
             Vector3 baricadePosition2 = new Vector3(-5 + baricadeAmount + 0.25f, -3.25f, -0.5f);
@@ -56,14 +73,45 @@ public class EnemyBehaviour : MonoBehaviour
             Instantiate(baricades, baricadePosition2, Quaternion.identity);
             Instantiate(baricades, baricadePosition3, Quaternion.identity);
             Instantiate(baricades, baricadePosition4, Quaternion.identity);
-        }
+        } 
+        //bool reachedRightBoundary = false;
+        //bool reachedLeftBoundary = false;
+        //
+        //foreach (GameObject enemyInvader in enemies)
+        //
+        //   enemyInvader.transform.Translate(Vector3.right * Time.deltaTime);
+        //   
+        //   if (enemyInvader.transform.position.x >= 7f)
+        //   {
+        //       reachedRightBoundary = true;
+        //       break;
+        //   }
+        //   if (enemyInvader.transform.position.x <= -7f)
+        //   {
+        //       reachedLeftBoundary = true;
+        //       break;
+        //   }
+        //
+        //
+        //if (reachedRightBoundary)
+        //
+        //   foreach (GameObject enemyInvader in enemies)
+        //   {
+        //       enemyInvader.transform.Translate(Vector3.left * Time.deltaTime);
+        //   }
+        //
+        //if (reachedLeftBoundary)
+        //
+        //   foreach (GameObject enemyInvader in enemies)
+        //   {
+        //       enemyInvader.transform.Translate(Vector3.right * Time.deltaTime);
+        //   }
+        //
     }
-
-    private void Update()
+    void Update()
     {
         UpdateUI();
     }
-
     void HandleEnemyDied(int pointsWorth)
     {
         score += pointsWorth;
